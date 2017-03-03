591b6371e0b1dfda4a595cda556068af48aa0731
LukashJ <lukash77@gmail.com>
2017-03-03 16:16:07 +0200

    ggg

--- src/OopWeek1/Task1ConcatString.java
+++ src/OopWeek1/Task1ConcatString.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Task1ConcatString {
 // fom git
     public static void main (String [] args) {
+
+        // La la тралала
         Scanner scanner = new Scanner(System.in);
             System.out.println ("Please enter string1:");
                 String string1 = scanner.nextLine();
