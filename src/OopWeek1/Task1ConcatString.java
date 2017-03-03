600787ce67513e69174a09cf4f20527d3bfc7953
LukashJ <lukash77@gmail.com>
2017-03-03 14:22:47 +0200

    Update Task1ConcatString.java

--- src/OopWeek1/Task1ConcatString.java
+++ src/OopWeek1/Task1ConcatString.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class Task1ConcatString {
 
     public static void main (String [] args) {
-
-        
         Scanner scanner = new Scanner(System.in);
             System.out.println ("Please enter string1:");
                 String string1 = scanner.nextLine();
